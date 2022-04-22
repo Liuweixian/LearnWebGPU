e16644673d8653d0a7b3ea1881726a6ba4471034
Ray Liu <65506466@qq.com>
2022-04-22 17:47:15 +0800

    finish the simple CMake

--- Samples/Advanced/build.sh
+++ Samples/Advanced/build.sh
@@ -5,6 +5,8 @@
 #       -G "MinGW Makefiles" (Windows)
 #       <path/to/CMakeLists.txt> # Note, pass in here ONLY the path to the file, not the filename 'CMakeLists.txt' itself.
 
-source ../../BuildTools/sh_export.sh 
-cmake -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN_ROOT/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -G "Unix Makefiles"
+source ../../BuildTools/sh_export.sh
+mkdir Build
+cmake -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN_ROOT/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -G "Unix Makefiles" ./CMakeLists.txt -B ./Build
+cd Build
 make

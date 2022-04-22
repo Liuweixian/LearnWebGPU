82f44bacd0f25fcc6c0bc01ac82cff25dbcf2f24
Ray Liu <65506466@qq.com>
2022-04-22 16:22:27 +0800

    Setting up advanced sample

---
+++ Samples/Advanced/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# cmake -DCMAKE_TOOLCHAIN_FILE=<EmscriptenRoot>/cmake/Modules/Platform/Emscripten.cmake
+#       -DCMAKE_BUILD_TYPE=<Debug|RelWithDebInfo|Release|MinSizeRel>
+#       -G "Unix Makefiles" (Linux and OSX)
+#       -G "MinGW Makefiles" (Windows)
+#       <path/to/CMakeLists.txt> # Note, pass in here ONLY the path to the file, not the filename 'CMakeLists.txt' itself.
+
+source ../../BuildTools/sh_export.sh 
+cmake -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN_ROOT/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -G "Unix Makefiles"
+make

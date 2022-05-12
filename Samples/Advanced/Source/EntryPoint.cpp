5d538787303b02aec3ec2c2fcbf27a8ae7912b4e
Ray Liu <65506466@qq.com>
2022-05-12 10:03:22 +0800

    Combine VBO & IBO class into RenderMeshBuffer

--- Samples/Advanced/Source/EntryPoint.cpp
+++ Samples/Advanced/Source/EntryPoint.cpp
@@ -19,11 +19,11 @@ int main(int argc, char **argv)
     // Create Material for RenderObject
     RenderMaterial *pMaterial = pRenderObject->CreateMaterial<RenderMaterial>(RenderPassIdx::DrawObjectPass);
     // Create VBO for RenderObject
-    RenderVertexBuffer *pRenderVertexBuffer = pRenderObject->CreateVertexBuffer<RenderVertexBuffer>();
+    RenderMeshBuffer *pRenderVertexBuffer = pRenderObject->CreateVertexBuffer<RenderMeshBuffer>();
     float vertexData[192] = {0.5, -0.5, 0.5, 0, 0, 1, 0, 0, -0.5, -0.5, 0.5, 0, 0, 1, 1, 0, 0.5, 0.5, 0.5, 0, 0, 1, 0, 1, -0.5, 0.5, 0.5, 0, 0, 1, 1, 1, 0.5, 0.5, -0.5, 0, 1, 0, 0, 1, -0.5, 0.5, -0.5, 0, 1, 0, 1, 1, 0.5, -0.5, -0.5, 0, 0, -1, 0, 1, -0.5, -0.5, -0.5, 0, 0, -1, 1, 1, 0.5, 0.5, 0.5, 0, 1, 0, 0, 0, -0.5, 0.5, 0.5, 0, 1, 0, 1, 0, 0.5, 0.5, -0.5, 0, 0, -1, 0, 0, -0.5, 0.5, -0.5, 0, 0, -1, 1, 0, 0.5, -0.5, -0.5, 0, -1, 0, 0, 0, 0.5, -0.5, 0.5, 0, -1, 0, 0, 1, -0.5, -0.5, 0.5, 0, -1, 0, 1, 1, -0.5, -0.5, -0.5, 0, -1, 0, 1, 0, -0.5, -0.5, 0.5, -1, 0, 0, 0, 0, -0.5, 0.5, 0.5, -1, 0, 0, 0, 1, -0.5, 0.5, -0.5, -1, 0, 0, 1, 1, -0.5, -0.5, -0.5, -1, 0, 0, 1, 0, 0.5, -0.5, -0.5, 1, 0, 0, 0, 0, 0.5, 0.5, -0.5, 1, 0, 0, 0, 1, 0.5, 0.5, 0.5, 1, 0, 0, 1, 1, 0.5, -0.5, 0.5, 1, 0, 0, 1, 0};
     pRenderVertexBuffer->SetData<float>(192, vertexData);
     // Create IBO for RenderObject
-    RenderIndexBuffer *pRenderIndexBuffer = pRenderObject->CreateIndexBuffer<RenderIndexBuffer>();
+    RenderMeshBuffer *pRenderIndexBuffer = pRenderObject->CreateIndexBuffer<RenderMeshBuffer>();
     uint16_t indexData[36] = {0, 2, 3, 0, 3, 1, 8, 4, 5, 8, 5, 9, 10, 6, 7, 10, 7, 11, 12, 13, 14, 12, 14, 15, 16, 17, 18, 16, 18, 19, 20, 21, 22, 20, 22, 23};
     pRenderIndexBuffer->SetData<uint16_t>(36, indexData);
 

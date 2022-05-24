4c4c340c80e3993748b71ec7bf50940767e1434b
Ray Liu <65506466@qq.com>
2022-05-24 18:47:37 +0800

    empty xcode project upload

---
+++ Samples/Advanced/WebGPURenderGraph/WebGPURenderGraph/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  WebGPURenderGraph
+//
+//  Created by 刘伟贤 on 2022/5/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
